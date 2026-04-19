b6a7aaecf7afc7db3b783f5ef963782fdde3d092
ronakjain1234 <ronakjain0919@utexas.edu>
2026-04-19 18:06:02 -0500

    Same model new classes

--- src/run_nn.c
+++ src/run_nn.c
@@ -31,30 +31,11 @@ void run_nn_infer(struct bma400_fifo_sensor_data *accel_data, uint16_t count)
 		return;
 	}
 
-	/* Map EI labels to index 0-6 */
-	// if (strcmp(label, "downstairs") == 0) biggest_idx = 0;
-	// else if (strcmp(label, "jump") == 0)       biggest_idx = 1;
-	// else if (strcmp(label, "running") == 0)    biggest_idx = 2;
-	// else if (strcmp(label, "sitting") == 0)    biggest_idx = 3;
-	// else if (strcmp(label, "standing") == 0)   biggest_idx = 4;
-	// else if (strcmp(label, "upstairs") == 0)   biggest_idx = 5;
-	// else if (strcmp(label, "walking") == 0)    biggest_idx = 6;
-
-	                   if (strcmp(label, "class 1") == 0)
-                       biggest_idx = 0;  /* 67     */
-                   else if (strcmp(label, "class 2") == 0)
-                       biggest_idx = 1;  /* clap   */
-                   else if (strcmp(label, "class 3") == 0)
-                       biggest_idx = 2;  /* dab    */
-                   else if (strcmp(label, "class 4") == 0)
-                       biggest_idx = 3;  /* disco  */
-                   else if (strcmp(label, "class 5") == 0)
-                       biggest_idx = 4;  /* floss  */
-                   else if (strcmp(label, "class 6") == 0)
-                       biggest_idx = 5;  /* griddy */
-                   else if (strcmp(label, "class 7") == 0)
-                       biggest_idx = 6;  /* roll   */
-                   else if (strcmp(label, "class 8") == 0)
-                       biggest_idx = 7;  /* wave   */
+	/* Map EI v5 labels to index 0-4 */
+	if      (strcmp(label, "class 1") == 0) biggest_idx = 0; /* clap      */
+	else if (strcmp(label, "class 2") == 0) biggest_idx = 1; /* idle      */
+	else if (strcmp(label, "class 3") == 0) biggest_idx = 2; /* roll      */
+	else if (strcmp(label, "class 4") == 0) biggest_idx = 3; /* six_seven */
+	else if (strcmp(label, "class 5") == 0) biggest_idx = 4; /* wave      */
 	else biggest_idx = 0;
 }
